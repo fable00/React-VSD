b20ada9b18611270ea3cf45d6c955b0d6a960a0a
simao00 <simaomcedo00@gmail.com>
2024-10-02 21:11:52 -0300

    Update 02/10/2024

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -6,13 +6,22 @@ import { getUserDetails } from "../../api/github"
 export const Dashboard = ()=>{
 
     const {username, setUsername} = useState('')
+    const {loading, setLoading} = useState(false)
 
     function handleSetUser(event) {
         setUsername(event.target.value)
     }
 
-    function handleGetDetails() {
-        
+    async function handleGetDetails() {
+        try {
+            setLoading(true)
+            const result = await getUserDetails(username)
+            console.log(result)
+        } catch (error) {
+            console.error(error.message)
+        } finally{
+            setLoading(false)
+        }
     }
 
     return(
